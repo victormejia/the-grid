b59bf1094e15babf92734d9a9d0966ac8fba63b7
Victor Mejia <victor.a.mejia@gmail.com>
2017-09-04 13:17:26 -0700

    script for seeding collection

---
+++ server/sample/seed.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const MongoClient = require('mongodb').MongoClient
+
+const hackers = require('./hackers')
+
+function seedCollection(collectionName, initialRecords) {
+
+  MongoClient.connect(process.env.DB_CONNECTION, (err, db) => {
+    console.log('connected to mongodb...')
+
+    const collection = db.collection(collectionName)
+
+    collection.remove()
+
+    console.log('inserting records...')
+
+    collection.insertMany(initialRecords, (err, result) => {
+      console.log(`${result.insertedCount} records inserted.`)
+      console.log('closing connection...')
+      db.close()
+      console.log('done.')
+    })
+
+  })
+}
+
+
+seedCollection('hackers', hackers)
