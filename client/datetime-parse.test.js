1e73666ebe185e6684f3b479a05dbd877abf1a2d
Victor Mejia <victor.a.mejia@gmail.com>
2017-09-05 10:27:51 -0700

    cleanup

--- client/datetime-parse.test.js
+++ client/datetime-parse.test.js
@@ -4,12 +4,12 @@ describe('DateTime Parser', () => {
   it('returned value should contain date format dd/mm/yyyy', () => {
     const isoString = '1960-06-01T11:01:12.720Z'
     expect(datetimeParse(isoString)).toContain('06/01/1960')
-  });
+  })
 
   it('returned value should contain time hh:mm[am|pm]', () => {
     const isoString = '1960-06-01T11:01:12.720Z'
     expect(datetimeParse(isoString)).toContain('11:01am')
-  });
+  })
 
   it('should convert ISO string to correct date format (am)', () => {
     const isoString = '1960-06-01T11:01:12.720Z'
